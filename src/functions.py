54ccb2e2c60a3fe5261355ad42e895fc6b7fa6b4
chabaniuk-m <chabanyuk.maks@gmail.com>
2024-04-30 20:06:30 +0300

    Changes by Kin

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,7 @@
 from main import *
 import random
+from data import *
+
 
 def get_random_word():
     with open(encoding="utf-8", file="../res/words.txt", mode='r') as file:
@@ -7,7 +9,7 @@ def get_random_word():
         return random.choice(random_words)
 
 
-def input_normal_letter():
+def input_normal_letter(list_: list):
     """
     - якщо користувач ввів символ не із українського алфавіту, то це "помилка"
     - якщо більше одного символа або 0 символів, то це "помилка"
@@ -21,19 +23,17 @@ def input_normal_letter():
     ukrainian_symbols_lower = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"
     player_letter = input("Введіть букву: ")
     if player_letter.lower() in ukrainian_symbols_lower and len(player_letter) == 1:
-        if player_letter not in list_of_all_player_letters:
-            list_of_all_player_letters.append(player_letter.lower())
+        if player_letter not in list_:
+            list_.append(player_letter.lower())
             return player_letter.lower()
         else:
             print("Ви вже вводили таку саму букву, прохання ввести нову букву.")
             print()
-            return input_normal_letter()
+            return input_normal_letter(list_)
     else:
         print("Необхідно ввести одну українську букву.")
         print()
-        return input_normal_letter()
+        return input_normal_letter(list_)
 
 
-if __name__ == '__main__':
-    letter = input_normal_letter()
-    print(f"Ви ввели букву: {letter}")
+
--- src/main.py
+++ src/main.py
@@ -18,13 +18,45 @@ from functions import *
 
 if __name__ == '__main__':
     # які змінні нам треба
+    list_of_all_player_letters = []
     computer_word = get_random_word()
-    letter = input_normal_letter()
+    letter = input_normal_letter(list_of_all_player_letters)
     lives = 8
     player_word = len(computer_word) * ["_"]
-    list_of_all_player_letters = []
 
     # код програми
 
-    ...
+    def check_letter():
+        print(letter)
+        print(computer_word)
+        for a in computer_word.split():
+            if a == letter:
+                print("djfdv")
+
+        if letter in computer_word:
+            return True
+        else:
+            return False
+
+
+    def func():
+        def func_():
+            letter = input_normal_letter(list_of_all_player_letters)
+            func()
+
+        print(f"Ви ввели букву: {letter}")
+        a = picture[lives].replace("$word", str("".join(player_word))).replace("$used", str(", ".join(list_of_all_player_letters)))
+        print(a)
+        func_()
+
+    if check_letter():
+        # for a in computer_word.split():
+        #     if a == letter:
+        #         a = letter
+        #         print(computer_word)
+
+        func()
+    else:
+        lives -= 1
+        func()
 
